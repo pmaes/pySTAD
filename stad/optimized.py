9b5b2fdaa689f6a78d078b9ce7878a5586166da6
Pieter Maes <pieter.maes.be@gmail.com>
2020-02-02 14:07:55 +0100

    Commenting optimized algorithm

--- stad/optimized.py
+++ stad/optimized.py
@@ -2,38 +2,86 @@ import numpy as np
 from scipy.sparse.csgraph import dijkstra, minimum_spanning_tree
 from scipy.linalg.blas import dgemm
 
+from stad import create_lensed_distmatrix_1step, assign_bins
+
+# This module aims to implement an optimized version of the base STAD algorithm.
+# There are three core ideas to achieve this:
+# 1. Operate directly on the adjacency matrix.
+#    All functions operate directly on the numpy adjacency matrix, saving us the
+#    roundtrip through python data structures.
+#
+# 2. Compute the All Pairs Shortest Paths faster.
+#    This the main bottleneck in computing the fitness of a given amount of
+#    edges. By implementing the Seidel algorithm, we get a big speed increase on
+#    dense graphs.
+
+# 3. Use an optimizing algorithm that requires less function evaluations.
+#    Simulated annealing and basin hopping assume fast objective functions and
+#    evaluate these liberally. LIPO is a global optimization algorithm that is
+#    efficient in function calls and requires little parameters.
+
+
+# Linear algebra libraries like BLAS tend to be optimized for float32.
+# We cast distance and adjacency matrices to make sure they are of this type.
 
 def unit_mst(distances):
+    """For a given distance matrix, returns the unit-distance MST."""
+    # Use toarray because we want a regular nxn matrix, not the scipy sparse matrix.
     mst = minimum_spanning_tree(distances).toarray()
+    # Set every edge weight to 1.
     mst = np.where(mst > 0, 1, 0)
+    # Symmetrize.
     mst += mst.T
     mst = mst.astype('float32')
     return mst
 
-
-def degrees(adj):
-    return np.repeat(np.sum(adj, axis=1)[None, :], adj.shape[0], axis=0)
-
+# These masking functions are utilities to ease the removal of the MST edges.
 
 def triu_mask(m, k=0):
+    """
+    For a given matrix m, returns like-sized boolean mask that is true for all
+    elements `k` offset from the diagonal.
+    """
     mask = np.zeros_like(m, dtype=np.bool)
     idx = np.triu_indices_from(m, k=k)
     mask[idx] = True
     return mask
 
 
-def masked_edges(m, mask):
-    return np.logical_and(triu_mask(m, k=1), mask)
+def masked_edges(adj, mask):
+    """
+    For a given adjacency matrix and a like-sized boolean mask, returns a mask
+    of the same size that is only true for the unqiue edges (the upper
+    triangle). Assumes a symmetrical adjacency matrix.
+    """
+    return np.logical_and(triu_mask(adj, k=1), mask)
 
 
 def ordered_edges(distances, mask):
+    """
+    For a given adjacency matrix with `n` edges and a like-sized mask, returns
+    an n x 2 array of edges sorted by distance.
+    """
+    # We are only interested in the indicies where our mask is truthy.
+    # On a boolean array nonzero returns the true indices.
+    # indices holds a tuple of arrays, one for each dimension.
     indices = np.nonzero(mask)
     ds = distances[indices]
+    # argsort returns the sorted indices of the distances.
+    # Note: these are not the same as the indices of our mask.
     order = np.argsort(ds)
-    return np.stack(indices, 1)[order].astype('int32')
+    # We wish to return a single array, so we use `stack` to combine the two nx1
+    # arrays into one nx2 array.
+    combined_indices = np.stack(indices, 1)
+    # Finally, we reorder our combined indices to be in the same order as the sorted distances.
+    return combined_indices[order].astype('int32')
 
 
-def py_with_edges(adj_m, edges):
+def with_edges(adj_m, edges):
+    """
+    For a given adjacency matrix and an nx2 array of edges, returns a new
+    adjacency matrix with the edges added. Symmetrizes the edges.
+    """
     new_adj = adj_m.copy()
     for edge in edges:
         x = edge[0]
@@ -44,6 +92,9 @@ def py_with_edges(adj_m, edges):
 
 
 def seidel(adj):
+    """Implements the Seidel algorithm for APSP on unweighted, undirected graphs.
+    For a given adjacency matrix, returns a like-sized matrix holding the
+    distances of the shortst paths between nodes."""
     n = adj.shape[0]
 
     # Prepare our base case to compare against, an n x n matrix that is 0 on the
@@ -52,6 +103,7 @@ def seidel(adj):
     np.fill_diagonal(base_case, 0)
     
     # Let Z = A . A
+    # Using DGEMM directly here instead of np.dot because the latter is 10x slower.
     z = dgemm(1, adj, adj)
 
     # Let B be an n x n binary matrix
@@ -79,15 +131,22 @@ def seidel(adj):
     # where d_ij =
     #   2*t_ij     if x_ij >= t_ij * degree(j)
     #   2*t_ij - 1 if x_ij <  t_ij * degree(j)
+    degrees = np.repeat(np.sum(adj, axis=1)[None, :], adj.shape[0], axis=0)
     d = np.where(x >= np.multiply(t, degrees(adj)), 2 * t, 2 * t - 1)
     return d.astype('float32')
 
 
+# The Seidel algorithm is significantly faster than BFS/Dijkstra on dense
+# graphs, but is slower on sparse ones. To pick between these somewhat
+# intelligently, we calculate the sparsity of the adjacency matrix and pick
+# Seidel if it's below a certain threshold.
+
 def sparsity(a):
     return 1.0 - (np.count_nonzero(a) / float(a.size))
 
-
+# A default of 0.9 seems to be close to the actual threshold on my system.
 def all_pair_shortest_paths(adj, method='auto', sparsity_threshold=0.9):
+    """Calculate the APSP for a given adjacency matrix."""
     if method == 'auto':
         s = sparsity(adj)
         # Naive
@@ -101,21 +160,39 @@ def all_pair_shortest_paths(adj, method='auto', sparsity_threshold=0.9):
     elif method == 'dijkstra':
         return dijkstra(adj).astype('float32')
 
+# We frame the STAD optimization as finding the optimal number of edges to add.
+# To evaluate the STAD objective function for a given number of edges we need
+# the high-dimensional distances to compare against, the MST to construct the
+# network from, and the sorted array of edges to add. This state is kept in the
+# STADObjective method. Because it implements __call__ it can be used as a
+# regular function. It might be helpful to think of it a as a closure over the
+# aforementioned bits of data.
+
 
 class STADObjective:
+    """The STAD objective function to maximalize"""
     def __init__(self, distances, mst, edges):
         self.distances = distances
         self.mst = mst
         self.edges = edges
 
+        # It's worth it to only compare the unique edges. The indexing isn't
+        # cheap, but indexing and calculating the correlation is still cheaper
+        # than calculating the correlation for the full matrix.
         self.triu_indices = np.triu_indices(distances.shape[0], k=1)
+        # We can cache the unique distances here, since they are constant.
         self.distance_vector = self.distances[self.triu_indices]
 
     def __call__(self, x):
+        # Force integer indices, so this objective can be used with general optimization methods.
         n = int(x)
-        adj = py_with_edges(self.mst, self.edges[:n])
+        # Construct the adjacency matrix with the given number of edges.
+        adj = with_edges(self.mst, self.edges[:n])
+        # Calculate the distance matrix thereof.
         dist = all_pair_shortest_paths(adj)
         dist = dist[self.triu_indices]
+        # Return the correlation between these distances and the high-dimensional ones.
+        # This means the function should be maximalised.
         return np.corrcoef(dist, self.distance_vector)[0][1]
 
 
@@ -230,7 +307,7 @@ def stad(distances, unit=False, debug=False):
     opt_obj = res['y'][opt_index]
     opt = int(res['x'][opt_index])
 
-    stad_adj = py_with_edges(mst, edges[:opt])
+    stad_adj = with_edges(mst, edges[:opt])
     if not unit:
         stad_adj = np.multiply(stad_adj, distances)
 
@@ -251,7 +328,7 @@ def run_stad(highD_dist_matrix, lens=[], features={}, debug=False):
     res = stad(highD_dist_matrix, debug=debug)
     if debug: print(res['opt'])
     g = igraph.Graph.Weighted_Adjacency(res['adj'].tolist())
-    
+
     distances = []
     for e in g.es():
         distances.append(highD_dist_matrix[e.source][e.target])
